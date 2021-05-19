696367ca19831801332dd195dd201de61b460a75
Reuben <2824016820@qq.com>
2021-05-19 16:22:38 +0800

    login2

--- NewsApp/LoginViewController.swift
+++ NewsApp/LoginViewController.swift
@@ -9,21 +9,51 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    let defaults = UserDefaults.standard
+    let nameKey = "Name"
+    let passwordKey = "Password"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    jumpToMain()
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var nameText: UITextField!
+    
+    @IBOutlet weak var passwordText: UITextField!
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        
+        let name = nameText.text!
+        let password = passwordText.text!
+        
+        defaults.set(name, forKey: nameKey)
+        defaults.set(password, forKey: passwordKey)
+        
+        jumpToMain()
+        
     }
-    */
+    func jumpToMain(){
+        let name_pre = defaults.string(forKey: nameKey)!
+        let password_pre = defaults.string(forKey: passwordKey)!
+        
+        if name_pre == "2019302110177" && password_pre == "123456"{
+            
+            
+            let mainBoard:UIStoryboard! = UIStoryboard(name: "Main", bundle: nil)
+            let VCMain = mainBoard!.instantiateViewController(withIdentifier: "vcMain")
+            UIApplication.shared.windows[0].rootViewController = VCMain
+        }
+        else{
+            passwordText.text = nil
+            passwordText.placeholder = "账号或密码错误！"
+        }
+        
+        
+    }
+    
 
 }
